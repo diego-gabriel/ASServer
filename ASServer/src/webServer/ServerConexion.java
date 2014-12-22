7962db360dec074dd5e61103b321537ef5fabadf
Adolf Mrls <fito.inzaghi@gmail.com>
2014-12-22 01:22:31 -0400

    servicio socketServer

--- ASServer/src/webServer/ServerConexion.java
+++ ASServer/src/webServer/ServerConexion.java
@@ -11,32 +11,45 @@ public class ServerConexion {
     private Socket socket;
     private DataOutputStream outPut;
     private String recivedMessage;
-
+    String[] arrayRequest;
     //inicio de servicio
     public void initServer() {
         BufferedReader entrada;
-
+        boolean activo = true;
         try {
             serverSocket = new ServerSocket(serverPort);
             socket = new Socket();
-            System.out.println("esperando una conexión:");
-            socket = serverSocket.accept();
-            System.out.println("un cliente se ha conectado!");
+            System.out.println("esperando una conexcion:");
+            
+                socket = serverSocket.accept();
+                System.out.println("un cliente se ha conectado!");
 
-            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            outPut = new DataOutputStream(socket.getOutputStream());
-            System.out.println("confirmando conexion al cliente....");
-            outPut.writeUTF("conexión exitosa... envia un mensaje ");
-            //Recepcion de mensaje
-            recivedMessage = entrada.readLine();
-            System.out.println(recivedMessage);
-            outPut.writeUTF("se recibio mensaje. Terminando conexion...");
-            outPut.writeUTF("gracias por conectarte, adios!");
-            System.out.println("cerrando conexión...");
-            serverSocket.close();//Aqui se cierra la conexión con el cliente
-        } catch (Exception e) {
+                entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                outPut = new DataOutputStream(socket.getOutputStream());
+                System.out.println("confirmando conexion al cliente....");
+            while (activo) {
+                outPut.writeUTF("request:");
+                //Recepcion de mensaje
+                recivedMessage = entrada.readLine();
+                arrayRequest =  (recivedMessage.trim()).split("\\s+");
+                
+                validarMensaje(arrayRequest);
+                
+                System.out.println(recivedMessage);
+                outPut.writeUTF("procesing...");
+            //outPut.writeUTF("gracias por conectarte, adios!");
+                //System.out.println("cerrando conexión...");
+                //if (recivedMessage.equalsIgnoreCase("exit")) {
+                    //outPut.writeUTF("byby");
+                    //serverSocket.close();
+                    //activo = false;
+                //}
+            }
+
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
     }
 
     public ServerSocket getServerSocket() {
@@ -46,4 +59,9 @@ public class ServerConexion {
     public void setServerSocket(ServerSocket serverSocket) {
         this.serverSocket = serverSocket;
     }
+    
+    public void validarMensaje(String[] mensaje){
+        
+    }
+    
 }
