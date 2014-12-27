395e1a62e1aabf9e3ae5d494aa16156d85217431
Adolf Mrls <fito.inzaghi@gmail.com>
2014-12-27 11:04:24 -0400

    test serverConexion

--- ASServer/test/ServerConexionTest.java
+++ ASServer/test/ServerConexionTest.java
@@ -1,13 +1,78 @@
+
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import webServer.ServerConexion;
 
 /**
  *
  * @author Adolfo
  */
 public class ServerConexionTest {
-    
-    public ServerConexionTest() {
+
+    @Test
+    public void testValidarTipoGet() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarTipo("get");
+        assertEquals(true, generado);
     }
-    
+
+    @Test
+    public void testValidarTipoHead() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarTipo("head");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidarTipoPost() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarTipo("post");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidarUrlConHttpDosPuntosSlashSlashWwwDireccion() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarUrl("http://www.misitio.com/index.html");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidarUrlWwwDireccion() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarUrl("www.misitio.com/index.html");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidarUrlSinWwwDireccion() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarUrl("ins/inicio/index.html");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidarVersionHttp1dot0() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarUrl("http/1.0");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidarVersionHttp1dot1() {
+        ServerConexion serverConexion = new ServerConexion();
+        boolean generado = serverConexion.validarUrl("http/1.1");
+        assertEquals(true, generado);
+    }
+
+    @Test
+    public void testValidRequest() {
+        ServerConexion serverConexion = new ServerConexion();
+        String recivedMessage = "get ins/inicio/index.html http/1.0";
+        String[] arrayMessage = recivedMessage.trim().split("\\s+");
+        boolean tipo = serverConexion.validarTipo(arrayMessage[0]);
+        boolean url = serverConexion.validarUrl(arrayMessage[1]);
+        boolean version = serverConexion.validarVersion(arrayMessage[2]);
+        assertEquals(true, tipo && url && version);
+    }
 }
