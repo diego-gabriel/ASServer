ca1e03562cbf612672bf870770abcbd142467b52
diego-gabriel <diego.gabriel.nd@gmail.com>
2014-12-27 16:51:07 -0400

    pulling

--- ASServer/src/StartServer.java
+++ ASServer/src/StartServer.java
@@ -13,7 +13,7 @@ public class StartServer {
     public static void main(String args[]){
         System.out.println("Starting server...");
         try {
-            new ServerSocket(80, new Server());
+            ServerSocket server = new ServerSocket(80, new Server());
         } catch (IOException ex) {
             Logger.getLogger(StartServer.class.getName()).log(Level.SEVERE, null, ex);
         }
